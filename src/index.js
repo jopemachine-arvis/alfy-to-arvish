44db185b06dfdfa6e4564d2ece721d1969897fa6
jopemachine <jopemachine@naver.com>
2021-09-17 00:16:20 +0900

    Implement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-const alfyToArvish = () => {
+const alfredToArvis = require('alfred-to-arvis');
+const arvisToAlfred = require('arvis-to-alfred');
+const execa = require('execa');
+const path = require('path');
+const loudRejection = require('loud-rejection');
+const { alfredToArvisCSV, arvisToAlfredCSV } = require('./path');
 
+loudRejection();
+
+const execaOpt = {
+  cwd: process.cwd()
+};
+
+const alfyToArvish = () => {
+  alfredToArvis(path.resolve(process.cwd(), 'info.plist')).then(() => {
+    execa('code-replacer', [
+      `--csv=${alfredToArvisCSV}`
+      `--dir=./`
+      `--dst=arvis`
+      `--template=\${source}->\${value}`
+    ], execaOpt);
+  });
 };
 
 const arvishToAlfy = () => {
-
+  arvisToAlfred(path.resolve(process.cwd(), 'arvis-workflow.json')).then(() => {
+    execa('code-replacer', [
+      `--csv=${arvisToAlfredCSV}`
+      `--dir=./`
+      `--dst=alfred`
+      `--template=\${source}->\${value}`
+    ], execaOpt);
+  });
 };
 
 module.exports = async (flags) => {
---
+++ src/path.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const alfredToArvisCSV = path.resolve(__dirname, 'alfred-to-arvis.csv');
+const arvisToAlfredCSV = path.resolve(__dirname, 'arvis-to-alfred.csv');
+
+module.exports = {
+  alfredToArvisCSV,
+  arvisToAlfredCSV,
+};
